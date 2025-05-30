91060f1f30ee5e33f523061a6cac405211bb38c8
Dante-web7 <dantecampos888@gmail.com>
2025-05-29 21:36:03 -0500

    Se agregó el proyecto

--- view/index.php
+++ view/index.php
@@ -158,7 +158,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
-      <a href="index3.html" class="brand-link">
+      <a href="index.php" class="brand-link">
         <img src="../assets/img/logo mixto.jpg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">IEP</span>
       </a>
