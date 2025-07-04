9213be5e6f84cd73f70ee061be309dbe9350e15c
dandev <dantecampos888@gmail.com>
2025-07-04 07:39:35 -0500

    test 22

--- view/MPDF/conexion.php
+++ view/MPDF/conexion.php
@@ -1,3 +1,12 @@
 <?php
-$mysql = new mysqli("db","admin","admin","sis_tramite");
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASSWORD');
+$db   = getenv('DB_NAME');
+
+$mysql = new mysqli($host, $user, $pass, $db);
+
+if ($mysql->connect_error) {
+    die("Error de conexión: " . $mysql->connect_error);
+}
 ?>
